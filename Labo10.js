b41ebeabd7c82a003559374b0fe14c237f4cdd68
JoseGomez <jose.gomez.ttc.228@unilibre.edu.co>
2024-10-30 18:47:11 -0500

    Primer punto

---
+++ Labo10.js
@@ -0,0 +1,8 @@
+//Primer punto
+console.log("Bienvenidos\n");
+console.log("Este es el primer punto...\n")
+let nombre = "Pedro"; let edad = 15; let esEstudiante = true;
+console.log("\nEl nombre es: ", nombre);
+console.log("\nLa edad es: ", edad);
+console.log("\nEs estudiante?", esEstudiante);
+//Segundo punto
